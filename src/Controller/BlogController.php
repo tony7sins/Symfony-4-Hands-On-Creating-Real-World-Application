9dcdb28e6fe1153a36d3850f4841874cc5f7a692
t7sg <tonigalczeskij@gmail.com>
2019-03-20 23:15:10 +0300

    11.Public and private services, service aliases

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Service\Greeting;
+use App\Service\VeryBadDesign;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,15 @@ class BlogController extends AbstractController
    * @var Greeting
    */
   private $greeting;
+  /**
+   * @var badDesign
+   */
+  private $badDesign;
 
-  public function __construct(Greeting $greeting)
+  public function __construct(Greeting $greeting, VeryBadDesign $badDesign)
   {
     $this->greeting = $greeting;
+    $this->badDesign = $badDesign;
   }
 
   /**
