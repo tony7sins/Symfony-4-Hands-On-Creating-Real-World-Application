bca3fd09a28a9f48d95f05ac3a8406eb095fb1ec
t7sg <tonigalczeskij@gmail.com>
2019-03-22 21:18:16 +0300

    19.Twig global vars

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,14 +2,32 @@
 
 namespace App\Twig;
 
+use Twig\Extension\GlobalsInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 /**
  *
  */
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements GlobalsInterface
 {
+  /**
+   * @var string
+   */
+  private $locale;
+
+  public function __construct(string $locale)
+  {
+      $this->locale = $locale;
+  }
+
+
+    public function getGlobals()
+    {
+      return [
+        'locale' => $this->locale,
+      ];
+    }
 
   public function getFilters()
   {
@@ -22,4 +40,5 @@ class AppExtension extends AbstractExtension
   {
     return '$' . number_format($number, 2, '.', ',');
   }
+
 }
