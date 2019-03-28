7a5b7f754c11df3e21a8140c8670f88bf8b55651
vagrant <vagrant@homestead>
2019-03-28 20:33:36 +0000

    37.Login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ *
+ */
+class SecurityController
+{
+
+  /**
+   * @var \Twig_Environment
+   */
+  private $twig;
+
+  public function __construct(\Twig_Environment $twig)
+  {
+    $this->twig = $twig;
+  }
+
+  /**
+   * @Route("/login", name="security_login")
+   */
+  public function login(AuthenticationUtils $authenticationUtils)
+  {
+    return new Response(
+        $this->twig->render(
+            'security/login.html.twig',
+            [
+                'last_username' => $authenticationUtils->getLastUsername(),
+                'error' => $authenticationUtils->getLastAuthenticationError(
+                ),
+            ]
+        )
+    );
+  }
+
+  /**
+   * @Route("/logout", name="security_logout")
+   */
+  public function logout()
+  {
+
+  }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,21 +4,55 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
 use App\Entity\MicroPost;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+      $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        for($i = 0; $i < 10; $i++)
-        {
-          $microPost = new MicroPost();
-          $microPost->setText('Some random text ' . rand(0, 100));
-          $microPost->setTime(new \DateTime('2019-03-25'));
-          $manager->persist($microPost);
-        }
-        $manager->flush();
+      $this->loadMicroPosts($manager);
+      $this->loadUsers($manager);
+    }
+
+    public function loadMicroPosts(ObjectManager $manager)
+    {
+      for($i = 0; $i < 10; $i++)
+      {
+        $microPost = new MicroPost();
+        $microPost->setText('Some random text ' . rand(0, 100));
+        $microPost->setTime(new \DateTime('2019-03-25'));
+        $manager->persist($microPost);
+      }
+      $manager->flush();
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+      $user = new User();
+      $user->setUsername('john_doe');
+      $user->setFullName('John Doe');
+      $user->setEmail('john_doe@gmail.com');
+      $user->setPassword(
+        $this
+          ->passwordEncoder->encodePassword(
+            $user,
+            'john123'
+          )
+      );
+
+      $manager->persist($user);
+      $manager->flush();
     }
 }
