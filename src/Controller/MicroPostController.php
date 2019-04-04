58525325c7cf640b4d8bafe627dc8533c53ee280
t7sg <tonigalczeskij@gmail.com>
2019-04-04 17:38:41 +0300

    50.Adding restricted posts

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 
 /**
@@ -139,11 +140,15 @@ class MicroPostController
 
   /**
    * @Route ("/add", name="micro_post_add")
+   * @Security("is_granted('ROLE_USER')")
    */
-  public function add(Request $request)
+  public function add(Request $request, TokenStorageInterface $tokenStorage)
   {
+    $user = $tokenStorage->getToken()->getUser();
+
     $microPost = new MicroPost();
     $microPost->setTime(new \DateTime());
+    $microPost->setUser($user);
 
     $form = $this->formFactory->create(
             MicroPostType::class,
